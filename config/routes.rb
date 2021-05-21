aa3da4376283595826d88f3e824f169f88e0f7f3
YutaKobari <syamoinu@gmail.com>
2021-05-21 22:18:29 +0900

    routes.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index'
+  get 'posts/csv_download', as: 'post_csv_download', defaults: { format: :csv }
   resources :posts,  only: [:index, :show]
   resources :brands, only: [:index, :show]
+  get 'accounts/csv_download', as: 'account_csv_download', defaults: { format: :csv }
   resources :accounts, only: [:index, :show] do
     resources :favorites, only: [:create, :destroy]
   end
+  get 'hash_tags/csv_download', as: 'hash_tag_csv_download', defaults: { format: :csv }
   resources :hash_tags, only: [:index]
+  get 'talk_posts/csv_download', as: 'talk_post_csv_download', defaults: { format: :csv }
   resources :talk_posts, only: [:index, :show]
+  get 'landing_pages/csv_download', as: 'landing_page_csv_download', defaults: { format: :csv }
   resources :landing_pages, only: [:index, :show]
+  get 'rich_menus/csv_download', as: 'rich_menu_csv_download', defaults: { format: :csv }
   resources :rich_menus, only: [:index, :show] do
     resources :rich_menu_contents, only: [] do
       get 'talk_flows', to: 'talk_flows#index'
@@ -16,4 +21,5 @@ Rails.application.routes.draw do
     end
   end
   post "append_modal", to: "brands#append_modal"
+  root 'top#index'
 end
